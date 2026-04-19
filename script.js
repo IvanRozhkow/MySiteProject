8b721bae050fa68e65cb31330c73288a4164ec12
IvanRozhkow <rozhkovivan2004@gmail.com>
2026-04-19 13:42:27 +0200

    Добавил кнопку для смены темы сайта

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ console.log("Мост между JS и HTML работает!");
 function sayHello() {
     alert("Привет! Ты нажал на кнопку. Это начало твоей логики на JS!");
 }
+
+const btn = document.getElementById('theme-toggle');
+
+btn.addEventListener('click', () => {
+    // Добавляем или убираем класс dark-mode у тега body
+    document.body.classList.toggle('dark-mode');
+    
+    // Меняем текст на кнопке
+    if (document.body.classList.contains('dark-mode')) {
+        btn.textContent = 'Светлая тема';
+    } else {
+        btn.textContent = 'Темная тема';
+    }
+});
